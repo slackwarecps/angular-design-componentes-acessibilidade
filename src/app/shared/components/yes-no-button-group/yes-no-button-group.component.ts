aa9003ec7e404d69e26fb021296fe61a55b8229f
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-06 11:01:35 -0300

    adicionada a logica de ativacao

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -9,7 +9,18 @@ export class YesNoButtonGroupComponent implements OnInit {
   @Input() public value: string = null;
   @Input() public label = '';
 
+  public options = YesNoButtonGroupOptions;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public activate(value: string): void {
+    this.value = value;
+  }
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no',
 }
